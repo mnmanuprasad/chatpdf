54c92ebdafa91628e048461c9c45ddac526565a2
mnmanuprasad <mnmanuprasad01@gmail.com>
2023-12-18 14:34:35 +0530

    Responsive ChatSideBar

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,3 +1,4 @@
+
 import ChatComponent from "@/components/ChatComponent";
 import ChatSideBar from "@/components/ChatSideBar";
 import PDFViewer from "@/components/PDFViewer";
@@ -11,52 +12,54 @@ import { Suspense } from "react";
 import { ChatSkelton } from "@/components/skeleton/ChatSkelton";
 
 type Props = {
-    params:{
-        chatId: string
-    }
+  params: {
+    chatId: string;
+  };
 };
 
-const ChatPage = async (props: Props)=>{
+const ChatPage = async (props: Props) => {
 
-    const {userId} = await auth();
+  const { userId } =  auth();
 
-    if(!userId){
-        return redirect("/sign-in");
-    }
+  if (!userId) {
+    return redirect("/sign-in");
+  }
 
-    const _chats = await db.select().from(chats).where(eq(chats.userId, userId))
-    
-    if(!_chats){
-        return redirect("/")
-    }
+  const _chats = await db.select().from(chats).where(eq(chats.userId, userId));
 
-    if(!_chats.find(chat=>chat.id===parseInt(props.params.chatId))){
-        return redirect("/")
-    }
+  if (!_chats) {
+    return redirect("/");
+  }
 
-    const currentChat = _chats.find(chat=> chat.id === parseInt(props.params.chatId))
+  if (!_chats.find((chat) => chat.id === parseInt(props.params.chatId))) {
+    return redirect("/");
+  }
 
-    return(
-        <div className="flex min-h-screen overflow-auto">
-            <div className="flex w-full max-h-screen overflow-auto">
-                {/* chat sidebar */}
-                <div className="flex-[1] max-w-xs">
-                   <ChatSideBar chats={_chats} chatId={parseInt(props.params.chatId)}/>
-                </div>
-                {/* PDF Viewer */}
-                <div className="hidden lg:block max-h-screen p-4 overflow-auto flex-[4]">
-                    <PDFViewer pdf_url={currentChat?.pdfUrl || ""} />
-                </div>
-                {/* Chat componenet */}
-                <div className="flex-[3] border-l-4 border-l-slate-400"> 
-                <Suspense fallback={<ChatSkelton />}>
-                    <ChatComponent  chatId={parseInt(props.params.chatId)}/>
-                </Suspense>
-                </div>
-            </div>
-        
+  const currentChat = _chats.find(
+    (chat) => chat.id === parseInt(props.params.chatId)
+  );
+
+  return (
+    <div className="flex min-h-screen overflow-auto">
+      <div className="flex w-full max-h-screen overflow-auto">
+        {/* chat sidebar */}
+        <div className="max-w-xs">
+          <ChatSideBar chats={_chats} chatId={parseInt(props.params.chatId)} />
         </div>
-    )
-}
 
-export default ChatPage
+        {/* PDF Viewer */}
+        <div className="hidden lg:block max-h-screen p-4 overflow-auto flex-[4] border-r-4 border-r-slate-400">
+          <PDFViewer pdf_url={currentChat?.pdfUrl || ""} />
+        </div>
+        {/* Chat componenet */}
+        <div className="flex-[3] ">
+          <Suspense fallback={<ChatSkelton />}>
+            <ChatComponent chatId={parseInt(props.params.chatId)} />
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -1,54 +1,104 @@
-"use client"
+"use client";
 
-import { DrizzleChat } from "@/lib/db/schema"
-import Link from "next/link"
-import React from "react"
-import { Button } from "./ui/button"
+import { DrizzleChat } from "@/lib/db/schema";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
 import { MessageCircle, PlusCircle } from "lucide-react";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { ChevronRight, ChevronLeft } from "lucide-react";
+import { useState,useEffect } from "react";
+import clsx from "clsx";
 
 type Props = {
-    chats: DrizzleChat[],
-    chatId: number
-}
+  chats: DrizzleChat[];
+  chatId: number;
+};
 
-const ChatSideBar = ({chats, chatId}: Props) => {
+const ChatSideBar = ({ chats, chatId }: Props) => {
+  
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  const [isChatSidebarOpen, setChatSidebarOpen] = useState(isMobile);
+
+  useEffect(() => {
+    if(window.innerWidth <= 768){
+      setIsMobile(true)
+    }
+  }, []);
+
+  function updateSideBarStatus() {
+    setChatSidebarOpen(!isChatSidebarOpen);
+  }
+
   return (
-    <div className="w-full min-h-screen max-h-screen overflow-auto soff p-4 text-gray-200 bg-gray-900">
-      <Link href="/">
-        <Button className="w-full border-dashed border-white border">
-          <PlusCircle className="mr-2 w-4 h-4" />
-          New Chat
-        </Button>
-      </Link>
-      
-      <div className="flex flex-col gap-2 mt-4">
-        {chats.map((chat)=>{
-          return(
-            <Link key={chat.id} href={`/chat/${chat.id}`}>
-                <div className={cn("rounded-lg p-3 text-slate-300 flex items-center",{
-                    "bg-blue-600 text-white": chat.id === chatId,
-                    "hover:text-white": chat.id !== chatId
-                })}>
-                    <MessageCircle className="mr-0"/>
-                    <p className="w-full overflow-hidden text-sm truncate whitespace-nowrap text-ellipsis">{chat.pdfName}</p>
+    <div className="flex justify-center min-h-screen">
+      <div
+        className={clsx(
+          "w-full min-h-screen max-h-screen overflow-auto  transition-[width] duration-1000 ease-in-out p-4 text-gray-200 bg-gray-900",
+          {
+            "w-0 hidden": isChatSidebarOpen == true,
+            // "w-60": isChatSidebarOpen == false
+          }
+        )}
+      >
+        <Link href="/">
+          <Button className="w-full border-dashed border-white border">
+            <PlusCircle className="mr-2 w-4 h-4" />
+            New Chat
+          </Button>
+        </Link>
+
+        <div className="flex flex-col gap-2 mt-4">
+          {chats.map((chat) => {
+            return (
+              <Link key={chat.id} href={`/chat/${chat.id}`}>
+                <div
+                  className={cn(
+                    "rounded-lg p-3 text-slate-300 flex items-center",
+                    {
+                      "bg-blue-600 text-white": chat.id === chatId,
+                      "hover:text-white": chat.id !== chatId,
+                    }
+                  )}
+                >
+                  <MessageCircle className="mr-0" />
+                  <p className="w-full overflow-hidden text-sm truncate whitespace-nowrap text-ellipsis">
+                    {chat.pdfName}
+                  </p>
                 </div>
-            </Link>
-          )
-        })
+              </Link>
+            );
+          })}
+        </div>
 
-        }
-      </div>
-
-      <div className="absolute bottom-4 left-4">
-        <div className="flex items-center gap-2 text-sm text-slate-500 flex-wrap">
+        <div className="absolute bottom-4 left-4">
+          <div className="flex items-center gap-2 text-sm text-slate-500 flex-wrap">
             <Link href={"/"}>Home</Link>
             <Link href={"/"}>Source</Link>
+          </div>
+          {/* Stripe Payment */}
         </div>
-        {/* Stripe Payment */}
+      </div>
+      <div className="text-slate-500 cursor-pointer self-center">
+        {isChatSidebarOpen && (
+          <ChevronRight
+            onClick={updateSideBarStatus}
+            className="animate-bounce"
+            size={32}
+          />
+        )}
+        {!isChatSidebarOpen && (
+          <ChevronLeft
+            onClick={updateSideBarStatus}
+            className="animate-bounce"
+            size={32}
+          />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatSideBar
+export default ChatSideBar;
